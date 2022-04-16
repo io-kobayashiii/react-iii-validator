76426daf276ecc54de198e6f38e26227e5babbb5
io-kobayashiii <io.kobayashiii@gmail.com>
2022-04-16 13:37:16 +0900

    add logging option

--- src/bin/types.ts
+++ src/bin/types.ts
@@ -2,6 +2,7 @@ export type Options = {
 	validationGroup?: HTMLElement
 	validClasses?: string[]
 	invalidClasses?: string[]
+	debugMode?: boolean
 }
 
 export type FormElements = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
